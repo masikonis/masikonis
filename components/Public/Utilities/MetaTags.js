b6ec0c65d53554a1880933a23013901e369d7883
Nerijus Masikonis <nerijus@masikonis.lt>
2020-04-14 15:04:16 +0300

    Add apple-touch-icon.png to the MetaTags component.

--- components/Public/Utilities/MetaTags.js
+++ components/Public/Utilities/MetaTags.js
@@ -2,7 +2,9 @@ import Head from 'next/head';
 
 const MetaTags = () => (
     <Head>
+        <meta name="theme-color" content="#ffffff" />
         <link rel="shortcut icon" href="/images/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/images/apple-touch-icon.png" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://cdnjs.cloudflare.com" />
         <link rel="preconnect" href="https://driftt.imgix.net" />
