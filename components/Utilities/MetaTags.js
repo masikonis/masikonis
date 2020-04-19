a191267c6b04df292bbb10b8e951f13522656847
Nerijus Masikonis <nerijus@masikonis.lt>
2020-04-19 21:30:55 +0300

    Update MetaTags.js
    Add google-site-verification meta tag to the head.

--- components/Utilities/MetaTags.js
+++ components/Utilities/MetaTags.js
@@ -4,6 +4,7 @@ const MetaTags = () => (
     <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
         <meta name="theme-color" content="#ffffff" />
+        <meta name="google-site-verification" content="MTux0sPksKjLtlBGwv9A8_0WLSEYDTeGX8zTxRGooB0" />
         <link rel="shortcut icon" href="/images/favicon.ico" />
         <link rel="apple-touch-icon" sizes="180x180" href="/images/apple-touch-icon.png" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
