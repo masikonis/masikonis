029fe82392c6c0d770495305318a91dca9fab45e
Nerijus Masikonis <nerijus@masikonis.lt>
2021-12-03 15:00:05 +0200

    Finish tweaking the mobile version of the home page.

--- components/Blocks/Welcome/Welcome.js
+++ components/Blocks/Welcome/Welcome.js
@@ -9,7 +9,7 @@ const Welcome = (props) => {
 					<div className="hidden md:block absolute top-0 right-0 opacity-25">
 						<Image src={illustration} width={415} height={313.96} alt="illustration" />
 					</div>
-					<h1 className="mt-6 font-serif text-4xl md:text-5xl">I'm Your Go-To Developer for<br />Everything Web-Related</h1>
+					<h1 className="mt-6 font-serif text-4xl md:text-5xl max-w-2xl">I'm Your Go-To Developer for Everything Web-Related</h1>
 					<h2 className="mt-2 font-sans text-lg md:text-xl">As a conversion optimization enthusiast, I improve websites to sell better.</h2>
 					<a href="#" className="inline-block bg-white p-5 mt-4 rounded text-sky-700 text-center font-sans font-bold uppercase">Work Experience</a>
 					<div className="block mt-5 opacity-25 md:hidden">
