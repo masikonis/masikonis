8013f3270e2d40c04f26604534b13f91e1b75e59
Nerijus Masikonis <nerijus@masikonis.lt>
2021-12-07 14:02:17 +0200

    Increase font size in the Qualities component.

--- components/Blocks/About/Qualities.js
+++ components/Blocks/About/Qualities.js
@@ -17,19 +17,19 @@ const Qualities = (props) => {
 			<ul className="mt-8">
 				<li className="box-border pl-20 mb-5 bg-no-repeat" style={{ backgroundImage: `url(${briefcaseIcon.src})` }}>
 					<strong className="block mb-1 font-sans text-white uppercase">Organised</strong>
-					<span className="font-sans text-sm text-white">I am highly organized, know how to prioritize, adapt quickly when the scope changes and thrive in a fast-paced environment with tight deadlines.</span>
+					<span className="font-sans text-sm xl:text-base text-white">I am highly organized, know how to prioritize, adapt quickly when the scope changes and thrive in a fast-paced environment with tight deadlines.</span>
 				</li>
 				<li className="box-border pl-20 mb-5 bg-no-repeat" style={{ backgroundImage: `url(${gearIcon.src})` }}>
 					<strong className="block mb-1 font-sans text-white uppercase">Flexible</strong>
-					<span className="font-sans text-sm text-white">I am not married to my ideas and I can shift gears easily when things change.</span>
+					<span className="font-sans text-sm xl:text-base text-white">I am not married to my ideas and I can shift gears easily when things change.</span>
 				</li>
 				<li className="box-border pl-20 mb-5 bg-no-repeat" style={{ backgroundImage: `url(${starIcon.src})` }}>
 					<strong className="block mb-1 font-sans text-white uppercase">Self-Motivated</strong>
-					<span className="font-sans text-sm text-white">I can work independently, I’m a self-starter and prefer mentorship over management.</span>
+					<span className="font-sans text-sm xl:text-base text-white">I can work independently, I’m a self-starter and prefer mentorship over management.</span>
 				</li>
 				<li className="box-border pl-20 mb-5 bg-no-repeat" style={{ backgroundImage: `url(${searchIcon.src})` }}>
 					<strong className="block mb-1 font-sans text-white uppercase">Methodical</strong>
-					<span className="font-sans text-sm text-white">All choices I make are based on strict research and adherence to industry standards.</span>
+					<span className="font-sans text-sm xl:text-base text-white">All choices I make are based on strict research and adherence to industry standards.</span>
 				</li>
 			</ul>
 		</div >
