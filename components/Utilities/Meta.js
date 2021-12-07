013afaf0098d80ae80ebc90db1d490f4cc58e581
Nerijus Masikonis <nerijus@masikonis.lt>
2021-12-07 10:39:51 +0200

    One more test.

--- components/Utilities/Meta.js
+++ components/Utilities/Meta.js
@@ -2,19 +2,24 @@ import Head from 'next/head';
 import appleTouchImg from '/public/images/apple-touch-icon.png';
 import coverImg from '/public/images/cover.png';
 
-const Meta = () => (
-	<Head>
-		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-		<meta name="theme-color" content="#ffffff" />
-		<meta name="google-site-verification" content="MTux0sPksKjLtlBGwv9A8_0WLSEYDTeGX8zTxRGooB0" />
-		<link rel="shortcut icon" href="/images/favicon.ico" />
-		<link rel="apple-touch-icon" sizes="180x180" href={appleTouchImg.src} />
-		<meta property="og:image" content={coverImg.src} />
-		<link rel="preconnect" href="https://fonts.googleapis.com" />
-		<link rel="preconnect" href="https://cdnjs.cloudflare.com" />
-		<link rel="preconnect" href="https://driftt.imgix.net" />
-		<link rel="preconnect" href="https://i.vimeocdn.com" />
-	</Head>
-);
+const Meta = (props) => {
+	const appleImage = 'https://masikonis.lt' + appleTouchImg.src;
+	const ogImage = 'https://masikonis.lt' + coverImg.src;
+
+	return (
+		<Head>
+			<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+			<meta name="theme-color" content="#ffffff" />
+			<meta name="google-site-verification" content="MTux0sPksKjLtlBGwv9A8_0WLSEYDTeGX8zTxRGooB0" />
+			<link rel="shortcut icon" href="/images/favicon.ico" />
+			<link rel="apple-touch-icon" sizes="180x180" href={appleImage} />
+			<meta property="og:image" content={ogImage} />
+			<link rel="preconnect" href="https://fonts.googleapis.com" />
+			<link rel="preconnect" href="https://cdnjs.cloudflare.com" />
+			<link rel="preconnect" href="https://driftt.imgix.net" />
+			<link rel="preconnect" href="https://i.vimeocdn.com" />
+		</Head>
+	);
+};
 
 export default Meta;
