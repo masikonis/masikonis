cc5738a336751914a78850c0de1e525b8d85ab08
Nerijus Masikonis <nerijus@masikonis.lt>
2021-12-07 10:36:02 +0200

    Update og:image for the social media sharing.

--- components/Utilities/Meta.js
+++ components/Utilities/Meta.js
@@ -1,4 +1,6 @@
 import Head from 'next/head';
+import appleTouchImg from '/public/images/apple-touch-icon.png';
+import coverImg from '/public/images/cover.png';
 
 const Meta = () => (
 	<Head>
@@ -6,8 +8,8 @@ const Meta = () => (
 		<meta name="theme-color" content="#ffffff" />
 		<meta name="google-site-verification" content="MTux0sPksKjLtlBGwv9A8_0WLSEYDTeGX8zTxRGooB0" />
 		<link rel="shortcut icon" href="/images/favicon.ico" />
-		<link rel="apple-touch-icon" sizes="180x180" href="/images/apple-touch-icon.png" />
-		<meta property="og:image" content="/images/logo-avatar.jpg" />
+		<link rel="apple-touch-icon" sizes="180x180" href={appleTouchImg.src} />
+		<meta property="og:image" content={coverImg.src} />
 		<link rel="preconnect" href="https://fonts.googleapis.com" />
 		<link rel="preconnect" href="https://cdnjs.cloudflare.com" />
 		<link rel="preconnect" href="https://driftt.imgix.net" />
